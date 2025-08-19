280e8f40e5e3cb9897424378f0924f77f9b5b23d
Bobakek <165632522+Bobakek@users.noreply.github.com>
2025-08-19 16:26:18 +0300

    Integrate ECS and networking into client main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { createScene } from './scene/engine'
+import { createWorld } from 'bitecs'
+import { NetClient } from './net/ws'
+import { babylonSyncSystem } from './ecs/systems/babylonSync'
+import { netApplySystem } from './ecs/systems/netApply'
 
+const world = createWorld()
+const net = new NetClient()
 
 function App() {
 const ref = React.useRef<HTMLCanvasElement>(null)
 React.useEffect(() => {
-if (ref.current) createScene(ref.current)
+if (ref.current) {
+const { engine, scene } = createScene(ref.current)
+const babylonSync = babylonSyncSystem(world)
+engine.runRenderLoop(() => {
+babylonSync()
+scene.render()
+})
+net.connect().then(() => {
+const ws = (net as any).ws as WebSocket
+const prev = ws.onmessage
+ws.onmessage = (ev) => {
+prev(ev)
+const snap = JSON.parse(ev.data)
+netApplySystem(world, { entities: snap.entities.map((e: any) => ({ id: e.id.toString(), x: e.x, y: e.y, z: e.z })) })
+}
+})
+}
 }, [])
 return <canvas ref={ref} style={{ width: '100vw', height: '100vh', display: 'block' }} />
 }
 
-
 createRoot(document.getElementById('root')!).render(<App />)
+
