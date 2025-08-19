30583e39eaf0c4aa67bc5d2fa14ea28f31aa5022
Bobakek <165632522+Bobakek@users.noreply.github.com>
2025-08-19 15:59:33 +0300

    feat: add renderable component

--- client/src/ecs/components.ts
+++ client/src/ecs/components.ts
@@ -2,5 +2,6 @@ import { defineComponent, Types } from 'bitecs'
 
 
 export const Transform = defineComponent({ x: Types.f32, y: Types.f32, z: Types.f32 })
+export const Renderable = defineComponent({ meshId: Types.ui32 })
 export const Velocity = defineComponent({ x: Types.f32, y: Types.f32, z: Types.f32 })
 export const PlayerTag = defineComponent()
