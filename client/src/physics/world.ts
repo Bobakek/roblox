b2d99022109c3b4c0b9d885aa0d96422f39addcb
Bobakek <165632522+Bobakek@users.noreply.github.com>
2025-08-21 23:54:27 +0300

    Add Rapier world and physics-based prediction

---
+++ client/src/physics/world.ts
@@ -0,0 +1,44 @@
+import { loadRapier } from './rapier'
+
+const worldReady = loadRapier().then(RAPIER => {
+  const world = new RAPIER.World({ x: 0, y: 0, z: 0 })
+  const minX = -100, maxX = 100
+  const minY = 0, maxY = 100
+  const minZ = -100, maxZ = 100
+  const halfX = (maxX - minX) / 2
+  const halfY = (maxY - minY) / 2
+  const halfZ = (maxZ - minZ) / 2
+  // left and right walls
+  world.createCollider(
+    RAPIER.ColliderDesc.cuboid(1, halfY, halfZ).setTranslation(minX - 1, halfY + minY, 0),
+    world.createRigidBody(RAPIER.RigidBodyDesc.fixed())
+  )
+  world.createCollider(
+    RAPIER.ColliderDesc.cuboid(1, halfY, halfZ).setTranslation(maxX + 1, halfY + minY, 0),
+    world.createRigidBody(RAPIER.RigidBodyDesc.fixed())
+  )
+  // bottom and top
+  world.createCollider(
+    RAPIER.ColliderDesc.cuboid(halfX, 1, halfZ).setTranslation(0, minY - 1, 0),
+    world.createRigidBody(RAPIER.RigidBodyDesc.fixed())
+  )
+  world.createCollider(
+    RAPIER.ColliderDesc.cuboid(halfX, 1, halfZ).setTranslation(0, maxY + 1, 0),
+    world.createRigidBody(RAPIER.RigidBodyDesc.fixed())
+  )
+  // front and back
+  world.createCollider(
+    RAPIER.ColliderDesc.cuboid(halfX, halfY, 1).setTranslation(0, halfY + minY, minZ - 1),
+    world.createRigidBody(RAPIER.RigidBodyDesc.fixed())
+  )
+  world.createCollider(
+    RAPIER.ColliderDesc.cuboid(halfX, halfY, 1).setTranslation(0, halfY + minY, maxZ + 1),
+    world.createRigidBody(RAPIER.RigidBodyDesc.fixed())
+  )
+  return { RAPIER, world }
+})
+
+export function getPhysicsWorld() {
+  return worldReady
+}
+
